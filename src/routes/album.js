c3f5cda1ac9ceef9e29dd464f4547c9c59c7965c
KiRist <37296174+KiRist-code@users.noreply.github.com>
2023-09-18 12:41:32 +0900

    [FEAT]Change image file from png to webp

--- src/routes/album.js
+++ src/routes/album.js
@@ -15,15 +15,17 @@ const Album = () => {
         <div id="list">
           {albums.map((item, index) => (
             <a href={item.link} rel="noreferrer" target="_blank">
-              <div id="album" key={index} style={
-                { backgroundImage: `url(/images/albums/${item.cover})` }
-              }/ >
+              <div
+                id="album"
+                key={index}
+                style={{ backgroundImage: `url(/images/albums/${item.cover})` }}
+              />
             </a>
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Album;
